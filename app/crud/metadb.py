9e92723aa9f93f8fc5486a396bafe28979377541
yooosungung <dulle2@gmail.com>
2024-02-07 14:35:49 +0900

    metadb fix

--- app/crud/metadb.py
+++ app/crud/metadb.py
@@ -1,7 +1,16 @@
-from typing import (Any, Dict, Iterator, List, Optional, Sequence, Tuple, Union,)
+from typing import (Any, Dict, Iterator, List, Optional, Literal, Sequence, Tuple, Union,)
 from sqlalchemy import URL
 
-from langchain.sql_database import SQLDatabase
+from langchain.sql_database import (SQLDatabase, truncate_word)
+
+def truncate_word(content: Any, *, length: int, suffix: str = "...") -> str:
+    if not isinstance(content, str) or length <= 0:
+        return content
+
+    if len(content) <= length:
+        return content
+
+    return content[: length - len(suffix)].rsplit(" ", 1)[0] + suffix
 
 class QueryMetaDB:
 
@@ -21,17 +30,43 @@ class QueryMetaDB:
             ) 
             self.__class__.db = SQLDatabase.from_uri(database_uri=url_object)
 
+    def run(
+        self,
+        command: str,
+        fetch: Literal["all", "one"] = "all",
+        include_columns: bool = False,
+    ) -> str:
+        """Execute a SQL command and return a string representing the results.
+
+        If the statement returns rows, a string of the results is returned.
+        If the statement returns no rows, an empty string is returned.
+        """
+        result = self.db._execute(command, fetch)
+
+        res = [
+            {
+                column: truncate_word(value, length=self._max_string_length)
+                for column, value in r.items()
+            }
+            for r in result
+        ]
+
+        if not include_columns:
+            res = [tuple(row.values()) for row in res]
+
+        return res
+        
     def get_system(self, id: str) -> str:
-        return self.db.run("select connect_type, connect_spec from api_system where system_id = {id} and status = 'Y'".format(id), "one")
+        return self.run("select connect_type, connect_spec from api_system where system_id = {id} and status = 'Y'".format(id), "one")
     
     def get_api(self, id: str) -> str:
-        return self.db.run("select system_id, api_spec from api_spec where api_id = {id} and status = 'Y'".format(id), "one")
+        return self.run("select system_id, api_spec from api_spec where api_id = {id} and status = 'Y'".format(id), "one")
     
     def get_prompt(self, id: str) -> str:
-        return self.db.run("select prompt_text from prompt where prompt_id = {id} and status = 'Y'".format(id), "one")
+        return self.run("select prompt_text from prompt where prompt_id = {id} and status = 'Y'".format(id), "one")
     
     def get_domain_list(self) -> Dict[str, Any]:
-        rows = self.db.run("select domain_id, domain_name, domain_desc from domain", "many")
+        rows = self.run("select domain_id, domain_name, domain_desc from domain", "many")
         return [{"domain_id":row[0], "domain_name":row[3], "domain_desc":row[3]} for row in rows]
 
     
